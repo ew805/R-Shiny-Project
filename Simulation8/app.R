d5e17b948dc499c6ed181cc11ffaa5ae02e2d135
Eleanor Ward <ew805@exeter.ac.uk>
2025-06-03 10:28:07 +0100

    different type of pages, tabset panel

---
+++ Simulation8/app.R
@@ -0,0 +1,26 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+
+
+ui <- fluidPage("Click counter",
+                 tabsetPanel(
+                 tabPanel("Counter",
+                          actionButton("count", "Click!")),
+                 tabPanel("Result",
+                          textOutput("Number")),
+))
+server <- function(input, output){
+  Number <- reactiveVal(0)
+  observeEvent(input$count, {Number(Number() + 1)})
+  output$Number <- renderText({paste(Number(), "clicks")})
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
